f1c72982b476b9b41c2cfb00469609d9a7f6cee8
kylekizirian <kyle.kizirian@gmail.com>
2017-02-21 17:52:43 -0500

    Update i2c.lua

--- i2c.lua
+++ i2c.lua
@@ -5,15 +5,28 @@ scl = 1
 -- initialize i2c, set pin1 as sda, set pin2 as scl
 i2c.setup(id, sda, scl, i2c.SLOW)
 
--- user defined function: read from reg_addr content of dev_addr
-function read_reg(dev_addr, reg_addr)
+-- user defined function: read single byte from reg_addr content of dev_addr
+function read_reg_byte(dev_addr, reg_addr)
     i2c.start(id)  -- initialize i2c
     i2c.address(id, dev_addr, i2c.TRANSMITTER) -- set device to xmit
     i2c.write(id, reg_addr) -- need to send register value that you want on the bus, after that you can read data
     i2c.stop(id)
     i2c.start(id)
     i2c.address(id, dev_addr, i2c.RECEIVER)
-    c = i2c.read(id, 4)
+    c = i2c.read(id, 1)
+    i2c.stop(id)
+    return c
+end
+
+-- user defined function: read multiple bytes from red_addr content of dev_addr
+function read_reg_bytes(dev_addr, reg_addr, bytes)
+    i2c.start(id)  -- initialize i2c
+    i2c.address(id, dev_addr, i2c.TRANSMITTER) -- set device to xmit
+    i2c.write(id, reg_addr) -- need to send register value that you want on the bus, after that you can read data
+    i2c.stop(id)
+    i2c.start(id)
+    i2c.address(id, dev_addr, i2c.RECEIVER)
+    c = i2c.read(id, bytes)
     i2c.stop(id)
     return c
 end
