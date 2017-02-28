cd57306a952db472d9455798539030fcca264b33
kylekizirian <kyle.kizirian@gmail.com>
2017-02-27 20:24:52 -0500

    added dofile() for register values

--- i2c.lua
+++ i2c.lua
@@ -1,24 +1,7 @@
 id  = 0
 sda = 2
 scl = 1
-OUT_X_L_XL = 0x28 -- reg address for lower 8-bits of accel in x 
-OUT_X_H_XL = 0x29 -- reg address for upper 8-bits of accel in x
-OUT_Y_L_XL = 0x2A -- accel, lower 8-bits in y
-OUT_Y_H_XL = 0x2B -- accel, upper 8-bits in y
-OUT_Z_L_XL = 0x2C -- accel, lower 8-bits in z
-OUT_Z_H_XL = 0x2D -- accel, upper 8-bits in z
-OUT_X_L_G = 0x18
-OUT_X_H_G = 0x19
-OUT_Y_L_G = 0x1A
-OUT_Y_H_G = 0x1B
-OUT_Z_L_G = 0x1C
-OUT_Z_H_G = 0x1D
-CTRL_REG1_G = 0x10
-CTRL_REG2_G = 0x11
-CTRL_REG3_G = 0x12
-CTRL_REG5_XL = 0x1F
-CTRL_REG6_XL = 0x20
-CTRL_REG7_XL = 0x21
+dofile("reg.lua")
 
 -- initialize i2c, set pin1 as sda, set pin2 as scl
 i2c.setup(id, sda, scl, i2c.SLOW)
@@ -53,17 +36,17 @@ function init_accel()
     -- set ctrl_reg5_xl
     register_write_value = 0
     register_write_value = bit.bor(register_write_value, bit.lshift(0x7, 3))
-    write_reg_byte(0x6B, CTRL_REG5_XL, register_write_value)
+    write_reg_byte(0x6B, reg.CTRL_REG5_XL, register_write_value)
 
     -- set ctrl_reg6_xl
     register_write_value = 0
     register_write_value = bit.lshift(bit.band(6, 0x07), 5)
     register_write_value = bit.bor(register_write_value, bit.lshift(0x3, 3))
-    write_reg_byte(0x6B, CTRL_REG6_XL, register_write_value)
+    write_reg_byte(0x6B, reg.CTRL_REG6_XL, register_write_value)
     
     -- set ctrl_reg7_xl
     register_write_value = 0
-    write_reg_byte(0x6B, CTRL_REG7_XL, register_write_value)
+    write_reg_byte(0x6B, reg.CTRL_REG7_XL, register_write_value)
     
 end
 
@@ -72,15 +55,15 @@ function init_gyro()
     -- set ctrl_reg1_g
     register_write_value = 0
     register_write_value = bit.lshift(bit.band(6, 0x07), 5)
-    write_reg_byte(0x6B, CTRL_REG1_G, register_write_value)
+    write_reg_byte(0x6B, reg.CTRL_REG1_G, register_write_value)
  
     -- set ctrl_reg2_g
     register_write_value = 0
-    write_reg_byte(0x6B, CTRL_REG2_G, register_write_value)
+    write_reg_byte(0x6B, reg.CTRL_REG2_G, register_write_value)
 
     -- set ctrl_reg3_g
     register_write_value = 0
-    write_reg_byte(0x6B, CTRL_REG3_G, register_write_value)
+    write_reg_byte(0x6B, reg.CTRL_REG3_G, register_write_value)
     
 end
 
@@ -104,27 +87,27 @@ function _read_imu_value(iic_address,lo_register, hi_register)
 end
 
 function read_x_accel()
-    return _read_imu_value(0x6B, OUT_X_L_XL, OUT_X_H_XL)
+    return _read_imu_value(0x6B, reg.OUT_X_L_XL, reg.OUT_X_H_XL)
 end
 
 function read_x_gyro()
-    return _read_imu_value(0x6B, OUT_X_L_G, OUT_X_H_G)
+    return _read_imu_value(0x6B, reg.OUT_X_L_G, reg.OUT_X_H_G)
 end
 
 function read_y_accel()
-    return _read_imu_value(0x6B, OUT_Y_L_XL, OUT_Y_H_XL)
+    return _read_imu_value(0x6B, reg.OUT_Y_L_XL, reg.OUT_Y_H_XL)
 end
 
 function read_y_gyro()
-    return _read_imu_value(0x6B, OUT_Y_L_G, OUT_Y_H_G)
+    return _read_imu_value(0x6B, reg.OUT_Y_L_G, reg.OUT_Y_H_G)
 end
 
 function read_z_accel()
-    return _read_imu_value(0x6B, OUT_Z_L_XL, OUT_Z_H_XL)
+    return _read_imu_value(0x6B, reg.OUT_Z_L_XL, reg.OUT_Z_H_XL)
 end
 
 function read_z_gyro()
-    return _read_imu_value(0x6B, OUT_Z_L_G, OUT_Z_H_G)
+    return _read_imu_value(0x6B, reg.OUT_Z_L_G, reg.OUT_Z_H_G)
 end
 
 init_accel()
