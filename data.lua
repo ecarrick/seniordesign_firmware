36218f7b2178402099568ca96de2ad25af1f153d
kylekizirian <kyle.kizirian@gmail.com>
2017-03-07 21:38:30 -0500

    added modularity to firebase_put
    added variable for name of JSON to put into firebase to make it more easily modifiable, also added function to increment shot number every time start() is called -> downside of this implementation though is this value isn't stored when ESP8266 power is cycled so it will overwrite shot0 the first time it is run after a power cycle.
    
    added line in setup to set wifi mode to Station

--- data.lua
+++ data.lua
@@ -1,10 +1,17 @@
 dofile("i2c.lua")
-
 dofile("wifi.lua")
+
+shot_num = 0
+
 function firebase_put(data)
 
+    -- set this to where you want to store each shot
+    json_name = "My_Test"
+    
+    firebase_str = "https://shotanalytics-17fc3.firebaseio.com/" .. json_name .. "/shot" .. shot_num .. ".json"
+    
     json = cjson.encode(data)
-    http.put("https://shotanalytics-17fc3.firebaseio.com/test/data.json",
+    http.put(firebase_str,
                 nil,
                 json,
                 function(code,data)
@@ -14,16 +21,24 @@ function firebase_put(data)
                   print(code, data)
     end
   end)
+    shot_num = shot_num + 1
 end
 
 function start()
-    data = record(4000, 100)
+    calib = calibrate(500)
+    
+    -- having trouble recording/sending more than about 20 samples
+    data = record(calib, 20)
 
     json = cjson.encode(data)
     print(json)
+    
+    --uncomment next line to send recording to firebase
+    --firebase_put(data)
 end
 
 function setup()
+    wifi.setmode(wifi.STATION)
     wifi.sta.config(wifi_ssid, wifi_password)
     while (wifi.sta.status() ~= 5 )
     do
@@ -35,7 +50,6 @@ function setup()
     
     
     start()
-
     
 end
 
