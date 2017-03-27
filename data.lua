22bfa432c8fb7a151a7dae66a7d1ab39aa9a0a38
kylekizirian <kyle.kizirian@gmail.com>
2017-03-27 18:55:23 -0400

    send recorded data 1 sample at a time
    instead of sending blocks of recorded data, modified code to read 1 sample from recorded data and then send to firebase. however, the http post function is non-blocking so it runs into issues when trying to send more than ~3 samples, need to figure out a way to wait for the http post to return before continuing execution

--- data.lua
+++ data.lua
@@ -2,15 +2,15 @@ dofile("i2c.lua")
 dofile("wifi.lua")
 
 shot_num = 0
+sample_num = 0
 
 function firebase_put(data)
-
     -- set this to where you want to store each shot
     json_name = "testdata"
     
-    firebase_str = "https://shotanalytics-17fc3.firebaseio.com/" .. json_name .. "/shot" .. shot_num .. ".json"
+    firebase_str = "https://shotanalytics-17fc3.firebaseio.com/" .. json_name .. "/shot" .. shot_num .. "/sample" .. sample_num .. ".json"
     print(firebase_str)
-    
+    print(data)
     http.post(firebase_str,
                 nil,
                 cjson.encode(data),
@@ -21,7 +21,7 @@ function firebase_put(data)
                   print(code, data)
     end
   end)
-    shot_num = shot_num + 1
+    sample_num = sample_num + 1
 end
 
 function start()
@@ -41,52 +41,40 @@ function start()
 end
 
 function start_and_record_to_file()
-    
+    sample_num = 1
     file_name = "test.txt"
     -- first parameter is number of values to record
-    record_to_file(5, file_name)
+    record_to_file(3, file_name)
     file.open(file_name)
 
-    firebase_put_count = 1
-    firebase_table = {}
     while true do
         line = file.readline()
         if (line == nil) then break
         end
         value_count = 1
         value_table = {}
-        -- parse values from text file, this regex extracts each integer
-        -- "-?" finds optional - sign in case integer is negative, "d+" fids integer and stops at next non integer char.,
-        -- in this case a comma
         for val in string.gmatch(line, "-?%d+") do
             value_table[value_count] = val
             value_count = value_count + 1
-            print(val)
+            --print(val)
         end
-        firebase_table[firebase_put_count] = value_table
-        firebase_put_count = firebase_put_count + 1
+        firebase_put(value_table)
         tmr.wdclr()
     end
-    firebase_put(firebase_table)
     file.close()
+    shot_num = shot_num + 1
 
-   
 end
 
 function setup()
     wifi.setmode(wifi.STATION)
-    wifi.sta.config(wifi_ssid, wifi_password)
+    wifi.sta.config(station_cfg.ssid, station_cfg.pwd)
     while (wifi.sta.status() ~= 5 )
     do
         print(wifi.sta.status())
         tmr.delay(1000000)
     end
-    
     print(wifi.sta.getip())
-    
-    
-    start()
-    
 end
 
-setup()
+
