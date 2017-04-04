c28605892aa988e096e31d3d0cf826e529039cba
Pmiller38 <pmiller38@gatech.edu>
2017-04-04 17:40:51 -0400

    Reads all data points from sensor at once

--- i2c.lua
+++ i2c.lua
@@ -47,29 +47,92 @@ end
 function read_z_gyro()
     return _read_imu_value(0x6B, reg.OUT_Z_L_G, reg.OUT_Z_H_G)
 
+out = {0,0,0,0,0,0}
 function read_imu_data(dev_addr)
-    data = {}
+    local data
     i2c.start(id)
     i2c.address(id, dev_addr, i2c.TRANSMITTER)
     i2c.write(id, reg.OUT_X_L_G)
     i2c.stop(id)
     i2c.start(id)
     i2c.address(id, dev_addr, i2c.RECEIVER)
-    data.X_L_G = i2c.read(id, 1)
-    data.X_H_G = i2c.read(id, 1)
-    data.Y_L_G = i2c.read(id, 1)
-    data.Y_H_G = i2c.read(id, 1)
-    data.Z_L_G = i2c.read(id, 1)
-    data.Z_H_G = i2c.read(id, 1)
-    data.X_L_X = i2c.read(id, 1)
-    data.X_H_X = i2c.read(id, 1)
-    data.Y_L_X = i2c.read(id, 1)
-    data.Y_H_X = i2c.read(id, 1)
-    data.Z_L_X = i2c.read(id, 1)
-    data.Z_H_X = i2c.read(id, 1)
+    data = i2c.read(id, 12)
     i2c.stop(id)
-    return data
-end  
+    
+    lower_byte = string.byte(data, 1)
+    upper_byte = string.byte(data, 2)
+
+    -- shift upper byte over and add lower byte
+    upper_byte = bit.lshift(upper_byte, 8)
+    out[1] = bit.bor(upper_byte, lower_byte)
+
+    -- convert unsigned to signed
+    if (out[1] > 2^15) then
+        out[1] = out[1] - 2^16
+    end
+
+    lower_byte = string.byte(data, 3)
+    upper_byte = string.byte(data, 4)
+
+    -- shift upper byte over and add lower byte
+    upper_byte = bit.lshift(upper_byte, 8)
+    out[2] = bit.bor(upper_byte, lower_byte)
+
+    -- convert unsigned to signed
+    if (out[2] > 2^15) then
+        out[2] = out[2] - 2^16
+    end
+
+    lower_byte = string.byte(data, 5)
+    upper_byte = string.byte(data, 6)
+
+    -- shift upper byte over and add lower byte
+    upper_byte = bit.lshift(upper_byte, 8)
+    out[3] = bit.bor(upper_byte, lower_byte)
+
+    -- convert unsigned to signed
+    if (out[3] > 2^15) then
+        out[3] = out[3] - 2^16
+    end
+
+    lower_byte = string.byte(data, 7)
+    upper_byte = string.byte(data, 8)
+
+    -- shift upper byte over and add lower byte
+    upper_byte = bit.lshift(upper_byte, 8)
+    out[4] = bit.bor(upper_byte, lower_byte)
+
+    -- convert unsigned to signed
+    if (out[4] > 2^15) then
+        out[4] = out[4] - 2^16
+    end
+
+    lower_byte = string.byte(data, 9)
+    upper_byte = string.byte(data, 10)
+
+    -- shift upper byte over and add lower byte
+    upper_byte = bit.lshift(upper_byte, 8)
+    out[5] = bit.bor(upper_byte, lower_byte)
+
+    -- convert unsigned to signed
+    if (out[5] > 2^15) then
+        out[5] = out[5] - 2^16
+    end
+
+    lower_byte = string.byte(data, 11)
+    upper_byte = string.byte(data, 12)
+
+    -- shift upper byte over and add lower byte
+    upper_byte = bit.lshift(upper_byte, 8)
+    out[6] = bit.bor(upper_byte, lower_byte)
+
+    -- convert unsigned to signed
+    if (out[6] > 2^15) then
+        out[6] = out[6] - 2^16
+    end
+    
+    return out
+end 
 
 -- user defined function: read single byte from reg_addr content of dev_addr
 function read_reg_byte(dev_addr, reg_addr)
@@ -212,22 +275,25 @@ function record(calib_val, nsamp)
 end
 
 function record_to_file(nsamp, file_name)
-    gain = 3
     count = 1
     if file.open(file_name, "w") then
         print("start\n")
+        local line = ""
+        local linecount = 1
         while count < nsamp do
             local temp = {}
-            temp[1] = read_x_accel()
-            temp[2] = read_y_accel()
-            temp[3] = read_z_accel()
-            temp[4] = read_x_gyro()
-            temp[5] = read_y_gyro()
-            temp[6] = read_z_gyro()
+            write_to_mux(0x01)
+            temp1 = read_imu_data(0x6B)
+            temp2 = read_imu_data(0x6B)
+            write_to_mux(0x02)
+            temp3 = read_imu_data(0x6B)
+            temp4 = read_imu_data(0x6B)
             tmr.wdclr()
-            --tmr.delay(1050)
             count = count + 1
-            line = "" .. temp[1] .. "," .. temp[2] .. "," .. temp[3] .. "," .. temp[4] .. "," .. temp[5] .. "," .. temp[6] .. ""
+            line = "" .. temp1[1] .. "," .. temp1[2] .. "," .. temp1[3] .. "," .. temp1[4] .. "," .. temp1[5] .. "," .. temp1[6] .. ","
+                      .. temp2[1] .. "," .. temp2[2] .. "," .. temp2[3] .. "," .. temp2[4] .. "," .. temp2[5] .. "," .. temp2[6] .. ","
+                      .. temp3[1] .. "," .. temp3[2] .. "," .. temp3[3] .. "," .. temp3[4] .. "," .. temp3[5] .. "," .. temp3[6] .. ","
+                      .. temp4[1] .. "," .. temp4[2] .. "," .. temp4[3] .. "," .. temp4[4] .. "," .. temp4[5] .. "," .. temp4[6] .. ""
             file.writeline(line)
         end
     end
