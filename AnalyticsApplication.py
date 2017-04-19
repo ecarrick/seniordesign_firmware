32974138d4fe59fb93b43680e9ef033534b2999e
mickealTaylor <mickeal.taylor@gmail.com>
2017-04-19 19:05:18 -0400

    Add files via upload

--- AnalyticsApplication.py
+++ AnalyticsApplication.py
@@ -51,16 +51,19 @@ class App:
         
         def simulate(self):
             text = self.label_txt.get()
-            print(self.label_txt.get())
+            text2 = self.save_txt.get()
+            print(text)
+            print(text2)
             #text = self.entryArea.get()
             if(text != ""):
-                pygame_start(text)
+                pygame_start(text, text2, 0)
         
         def save(self):
             text = self.label_txt.get()
             print("Now saving " + text)
             if(text != ""):
                 saveShot(text)
+           
         
         def dHHandler(self):
             a = dataHandle()
@@ -93,7 +96,7 @@ def dataHandle():
     global saveArr
     #link = input("Firebase link: ")
     fb = fbtf.establish_connection('https://shotanalytics-17fc3.firebaseio.com/')
-    shotArr = fb.get("/DataSession29",None)
+    shotArr = fb.get("/DataSession28",None)
     saveArr = fb.get("/SavedShots", None)
     print("This ran")
     return [shotArr, saveArr]
@@ -124,7 +127,7 @@ def get_doubleIntegral_array(data, sensor):
     pos = fbtf.integral2(vel, time[:len(time)-1])
     return pos
     
-def pygame_draw(arr, screen, axis, ind, color, maxMax):
+def pygame_draw(arr, screen, axis, ind, color, maxMax, offset):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
                 done = True
@@ -139,7 +142,7 @@ def pygame_draw(arr, screen, axis, ind, color, maxMax):
         if(max(arr[2]) ==0):
             max2=500000000
             #print("this is max2:" + str(max0))
-    xVal = (((arr[axis][ind])/max0)*300)+300
+    xVal = (((arr[axis][ind]+offset)/max0)*300)+300
     yVal = (((arr[2][ind])/max2)*-300)+300
     xValInt = int(xVal.astype(int))
     yValInt = int(yVal.astype(int))
@@ -158,7 +161,8 @@ def calcDistance(x1, y1, x2, y2):
     val = pow((pow((x1-x2),2) + pow((y1-y2),2)), .5)
     return val
 
-def pygame_start(t):
+def pygame_start(t, s, orien):
+    print(t, s)
     init_pygame()
     BLACK = (0,0,0)
     WHITE = (255, 255, 255)
@@ -168,13 +172,28 @@ def pygame_start(t):
     font = pygame.font.SysFont('Calabria', 20,True, False)
     global pos
     pos = get_doubleIntegral_array(shotArr[t], 0)
+    pos1 = get_doubleIntegral_array(shotArr[t], 1)
+    pos2 = get_doubleIntegral_array(shotArr[t], 2)
+    pos3 = get_doubleIntegral_array(shotArr[t], 3)
     x=0
+    savePlot = (s!= "")
+    minVal = min(len(pos[0]),len(pos1[0]),len(pos2[0]),len(pos3[0]))
+    if(savePlot):
+        print("Keys: " + str(saveArr.keys()))
+        Spos = get_doubleIntegral_array(saveArr[s], 0)
+        Spos1 = get_doubleIntegral_array(saveArr[s], 1)
+        Spos2 = get_doubleIntegral_array(saveArr[s], 2)
+        Spos3 = get_doubleIntegral_array(saveArr[s], 3)
+        minVal = min(len(Spos[0]),len(Spos1[0]),len(Spos2[0]),
+                     len(Spos3[0]), minVal)
     distArr = []
-    while x<len(pos[0]):
+    while x<minVal:
         screen.fill(WHITE)
-        maxX= max(pos[0])-min(pos[0])
-        maxY = max(pos[1])-min(pos[1])
-        maxMax = max([maxX, maxY])
+        max0 = max(pos[orien])-min(pos[orien])+.20
+        max1 = max(pos1[orien])-min(pos1[orien])+.10
+        max2 = max(pos2[orien])-min(pos2[orien])
+        max3 = max(pos3[orien])-min(pos3[orien])-.10
+        maxMax = max([max0, max1, max2, max3])
         pygame.draw.line(screen, BLACK, [0,300],[600,300], 5)
         pygame.draw.line(screen, BLACK, [300,0],[300,600], 5)
         pygame.draw.line(screen, BLACK, [300,200], [305,200], 5)
@@ -189,8 +208,20 @@ def pygame_start(t):
         screen.blit(text1, [400, 290])
         text2 = font.render("cm", True, BLACK)
         screen.blit(text2, [570, 310])
-        ball1 = pygame_draw(pos, screen, 0, x, RED, maxMax)
-        ball2 = pygame_draw(pos, screen, 1, x, BLUE, maxMax)
+        ball1 = pygame_draw(pos, screen, orien, x, RED, maxMax, .20)
+        ball2 = pygame_draw(pos1, screen, orien, x, RED, maxMax, .10)
+        ball3 = pygame_draw(pos2, screen, orien, x, RED, maxMax, 0)
+        ball4 = pygame_draw(pos3, screen, orien, x, RED, maxMax, -0.10)
+        if(savePlot):
+            smax0 = max(Spos[orien])-min(Spos[orien])+.20
+            smax1 = max(Spos1[orien])-min(Spos1[orien])+.10
+            smax2 = max(Spos2[orien])-min(Spos2[orien])
+            smax3 = max(Spos3[orien])-min(Spos3[orien])-.10
+            smaxMax = max([smax0, smax1, smax2, smax3])
+            sball1 = pygame_draw(Spos, screen, orien, x, BLUE, smaxMax, .20)
+            sball2 = pygame_draw(Spos1, screen, orien, x, BLUE, smaxMax, .10)
+            sball3 = pygame_draw(Spos2, screen, orien, x, BLUE, smaxMax, 0)
+            sball4 = pygame_draw(Spos3, screen, orien, x, BLUE, smaxMax, -0.10)            
         text3 = font.render(str(round(ball1[2])) + ", " + str(round(ball1[3])), True, RED)
         screen.blit(text3, [ball1[0], ball1[1]+15])
         text4 = font.render(str(round(ball2[2])) + ", " + str(round(ball2[3])), True, RED)
